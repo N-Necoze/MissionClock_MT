66b4b67d7c6485a6c036d0054f5c122efff0eac4
N-Necoze <n.nishidaneco@gmail.com>
2022-03-06 14:39:04 +0900

    commit

--- mission_clocke_MT.py
+++ mission_clocke_MT.py
@@ -37,73 +37,73 @@ target2_2 = datetime.datetime(year_2, month_2, day_2, hour_2, min_2)
 date2_t1 = target2_2.strftime("%Y/%m/%d")
 date2_t2 = target2_2.strftime("%Y/%m/%d %H:%M:%S")
 
+
 class Application(tk.Frame):
-    def __init__(self,master):
+    def __init__(self, master):
         super().__init__(master)
         # メインウィンドウサイズ
         master.geometry("1850x640")
         master.title("〇〇作戦アプリ")
         Label = tk.Label
         self.pack()
-        
         # Countdown_1
-        self.Countdown_1=Label(self, bg="black", font=("",50), text="Countdown" ,fg="#FF9933")
-        self.Countdown_1.grid(row=0, column=1 ,padx=(0, 200),sticky="news")
+        self.Countdown_1 = Label(self, bg="black", font=("", 50), text="Countdown", fg="#FF9933")
+        self.Countdown_1.grid(row=0, column=1, padx=(0, 200), sticky="news")
         # [期限名1]
-        self.Countdown_time_1=Label(self, bg="black", font=("ToppanBunkyuMidashiMinchoStdN-ExtraBold",60,"bold"), text=kigen_name_1 ,fg="#FF9933")
+        self.Countdown_time_1=Label(self, bg="black", font=("ToppanBunkyuMidashiMinchoStdN-ExtraBold", 60, "bold"), text=kigen_name_1, fg="#FF9933")
         self.Countdown_time_1.grid(row=1, column=0, sticky="news")
         # [期限日付表示_1]
-        self.wt1=Label(self, bg="black", font=("DSEG7 Classic", 80, "bold") ,fg="#FF9933")
+        self.wt1=Label(self, bg="black", font=("DSEG7 Classic", 80, "bold"), fg="#FF9933")
         self.wt1.grid(row=1, column=1, sticky="news")
         # [期限時刻表示_1]
-        self.wt1_1=Label(self, bg="black", font=("DSEG7 Classic", 80, "bold") ,fg="#FF9933")
-        self.wt1_1.grid(row=1, column=2 ,padx=(0, 300), sticky="news")
+        self.wt1_1=Label(self, bg="black", font=("DSEG7 Classic", 80, "bold"), fg="#FF9933")
+        self.wt1_1.grid(row=1, column=2, padx=(0, 300), sticky="news")
         # [英語表記]
-        self.Countdown_UN_1=Label(self, bg="black", font=("",30), text=text_1 ,fg="#FF9933")
+        self.Countdown_UN_1=Label(self, bg="black", font=("", 30), text=text_1, fg="#FF9933")
         self.Countdown_UN_1.grid(row=2, column=0, sticky="news")
         # [ラベル(Day)]
-        self.Countdown_UN_1=Label(self, bg="black", font=("",30), text="Day" ,fg="#FF9933")
+        self.Countdown_UN_1=Label(self, bg="black", font=("", 30), text="Day", fg="#FF9933")
         self.Countdown_UN_1.grid(row=2, column=1, sticky="news")
         # [ラベル(Time)]
-        self.Countdown_UN_1=Label(self, bg="black", font=("",30), text="Time" ,fg="#FF9933")
+        self.Countdown_UN_1=Label(self, bg="black", font=("", 30), text="Time", fg="#FF9933")
         self.Countdown_UN_1.grid(row=2, column=2, sticky="news")
-        
+
         # Countdown_2
-        self.Countdown_2=Label(self, bg="black", font=("",50), text="Countdown" ,fg="#FF9933")
-        self.Countdown_2.grid(row=3, column=1 ,padx=(0, 200) ,sticky="news")
+        self.Countdown_2=Label(self, bg="black", font=("", 50), text="Countdown", fg="#FF9933")
+        self.Countdown_2.grid(row=3, column=1, padx=(0, 200), sticky="news")
         # [期限名2]
-        self.Countdown_time_2=Label(self, bg="black", font=("ToppanBunkyuMidashiMinchoStdN-ExtraBold",60,"bold"), text=kigen_name_2 ,fg="#FF9933")
-        self.Countdown_time_2.grid(row=4, column=0, padx=(0, 50),sticky="news")
+        self.Countdown_time_2=Label(self, bg="black", font=("ToppanBunkyuMidashiMinchoStdN-ExtraBold", 60, "bold"), text=kigen_name_2, fg="#FF9933")
+        self.Countdown_time_2.grid(row=4, column=0, padx=(0, 50), sticky="news")
         # [期限日付表示_2]
-        self.wt2=Label(self, bg="black", font=("DSEG7 Classic", 80, "bold") ,fg="#FF9933")
+        self.wt2=Label(self, bg="black", font=("DSEG7 Classic", 80, "bold"), fg="#FF9933")
         self.wt2.grid(row=4, column=1, sticky="news")
         # [期限時刻表示_2]
-        self.wt2_1=Label(self, bg="black", font=("DSEG7 Classic", 80, "bold") ,fg="#FF9933")
-        self.wt2_1.grid(row=4, column=2 ,padx=(0, 300), sticky="news")
+        self.wt2_1=Label(self, bg="black", font=("DSEG7 Classic", 80, "bold"), fg="#FF9933")
+        self.wt2_1.grid(row=4, column=2, padx=(0, 300), sticky="news")
         # [英語表記]
-        self.Countdown_UN_2=Label(self, bg="black", font=("",30), text=text_2 ,fg="#FF9933")
-        self.Countdown_UN_2.grid(row=5, column=0,sticky="news")
+        self.Countdown_UN_2=Label(self, bg="black", font=("", 30), text=text_2, fg="#FF9933")
+        self.Countdown_UN_2.grid(row=5, column=0, sticky="news")
         # [ラベル(Day)]
-        self.Countdown_UN_2=Label(self, bg="black", font=("",30), text="Day" ,fg="#FF9933")
+        self.Countdown_UN_2=Label(self, bg="black", font=("", 30), text="Day", fg="#FF9933")
         self.Countdown_UN_2.grid(row=5, column=1, sticky="news")
         # [ラベル(Time)]
-        self.Countdown_UN_2=Label(self, bg="black", font=("",30), text="Time" ,fg="#FF9933")
+        self.Countdown_UN_2=Label(self, bg="black", font=("", 30), text="Time", fg="#FF9933")
         self.Countdown_UN_2.grid(row=5, column=2, sticky="news")
-        
+
         # --- 日本標準時 ---
-        self.Countdown_3=Label(self, bg="black", font=("",50), text="Live" ,fg="#FF9933")
-        self.Countdown_3.grid(row=9, column=1 ,padx=(0, 350) ,sticky="news")
+        self.Countdown_3=Label(self, bg="black", font=("", 50), text="Live", fg="#FF9933")
+        self.Countdown_3.grid(row=9, column=1, padx=(0, 350), sticky="news")
         # [日本標準時]
-        self.Countdown_time_3=Label(self, bg="black", font=("ToppanBunkyuMidashiMinchoStdN-ExtraBold",60,"bold"), text="日本標準時" ,fg="#FF9933")
-        self.Countdown_time_3.grid(row=10, column=0, padx=(0, 120),sticky="news")
+        self.Countdown_time_3=Label(self, bg="black", font=("ToppanBunkyuMidashiMinchoStdN-ExtraBold", 60, "bold"), text="日本標準時", fg="#FF9933")
+        self.Countdown_time_3.grid(row=10, column=0, padx=(0, 120), sticky="news")
         # [期限時刻表示]
-        self.wt3=Label(self, bg="black", font=("DSEG7 Classic", 80, "bold") ,fg="#FF9933")
+        self.wt3=Label(self, bg="black", font=("DSEG7 Classic", 80, "bold"), fg="#FF9933")
         self.wt3.grid(row=10, column=1)
-        self.wt3_1=Label(self, bg="black", font=("DSEG7 Classic", 80, "bold") ,fg="#FF9933")
-        self.wt3_1.grid(row=10 ,padx=(0, 300), column=2)
+        self.wt3_1=Label(self, bg="black", font=("DSEG7 Classic", 80, "bold"), fg="#FF9933")
+        self.wt3_1.grid(row=10, padx=(0, 300), column=2)
         # [英語表記]
-        self.Countdown_UN_3=Label(self, bg="black", font=("",30), text="Japan Standard Time" ,fg="#FF9933")
-        self.Countdown_UN_3.grid(row=11, column=0, padx=(0, 120),sticky="news")
+        self.Countdown_UN_3=Label(self, bg="black", font=("", 30), text="Japan Standard Time", fg="#FF9933")
+        self.Countdown_UN_3.grid(row=11, column=0, padx=(0, 120), sticky="news")
         
         master.after(50, self.update)
     def update(self):
